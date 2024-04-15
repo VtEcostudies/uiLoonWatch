4d2cbefa664a64246be36c630c14714da63dd003
jloomisVCE <jloomis@vtecostudies.org>
2024-04-15 13:45:07 -0400

    use remote fetchGoogleSheetsData instead

--- js/googleSheetSignups.js
+++ js/googleSheetSignups.js
@@ -1,4 +1,5 @@
 //import { fetchGoogleSheetData } from '../VAL_Web_Utilities/js/fetchGoogleSheetsData.js';
+import { fetchGoogleSheetData } from 'https://vtatlasoflife.org/VAL_Web_Utilities/js/fetchGoogleSheetsData.js';
 
 export let sheetIds = {
     loonMonitorSignUps: '1ZSczQ00dj2ku0eY0xcjUBQM-CD2Iu3CiE-wwtbUzk7I',
@@ -51,31 +52,6 @@ export async function getLoonWatchSignups(sheetNumber=0) {
     return {'lake':lake,'user':user};
     } catch(err) {
         console.log(`getLoonWatchSignups(${sheetNumber}) ERROR:`, err);
-        return Promise.reject(err);
-    }
-}
-/*
-    Fetch a single Google sheet's data by sheet ID and ordinal sheet number.
-*/
-export async function fetchGoogleSheetData(spreadsheetId=sheetIds.loonWatchSignUps, sheetNumber=0) {
-    let apiUrl = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/?key=${googleApiKey}&includeGridData=true`;
-
-    try {
-        let res = await fetch(apiUrl);
-        //console.log(`fetchGoogleSheetData(${spreadsheetId},${sheetNumber}) RAW RESULT:`, res);
-        if (res.status > 299) {return res;}
-        let json = await res.json();
-        //console.log(`fetchGoogleSheetData(${spreadsheetId}) JSON RESULT:`, json);
-        let prop = json.sheets[sheetNumber].properties;
-        let head = json.sheets[sheetNumber].data[0].rowData[0].values;
-        let data = json.sheets[sheetNumber].data[0].rowData.slice(1);
-        //console.log(`Sheet-${sheetNumber} properties:`, prop);
-        //console.log(`Sheet-${sheetNumber} row header:`, head);
-        //console.log(`Sheet-${sheetNumber} row data:`, data);
-        return {'properties':prop, 'head':head, 'rows':data};
-    } catch (err) {
-        console.log(`fetchGoogleSheetData(${spreadsheetId}) ERROR:`, err);
-        //return new Error(err)
         return Promise.reject(err);
     }
 }
